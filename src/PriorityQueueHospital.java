10a22bd0455dcab7b558dec466476d12d27388d7
Aaron Craig <aaron.t.craig-1@ou.edu>
2019-03-07 11:53:18 -0600

    started priorityQueue, finished constructor

--- src/PriorityQueueHospital.java
+++ src/PriorityQueueHospital.java
@@ -1,5 +1,54 @@
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 
-public class PriorityQueueHospital
+public class PriorityQueueHospital<PatientType> extends Hospital<PatientType>
 {
+    
+    PriorityQueue<PatientType> priorityHospital;
+    public PriorityQueueHospital() {
+        priorityHospital = new PriorityQueue<PatientType>();
+    }
+    
+    @Override
+    public void addPatient(PatientType patient)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public PatientType nextPatient()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public PatientType treatNextPatient()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int numPatients()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String hospitalType()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String allPatientInfo()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
