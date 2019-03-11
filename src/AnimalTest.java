327037146cded412558904a1fa40ad54728576d6
Aaron Craig <aaron.t.craig-1@ou.edu>
2019-03-10 23:38:58 -0500

    wrote animal test class

--- src/AnimalTest.java
+++ src/AnimalTest.java
@@ -1,5 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
 
 public class AnimalTest
 {
-
+    public void animalTest(){
+        Animal ani = new Animal("doggo", 12);
+        Assert.assertEquals("Getbreed is incorrect", "doggo", ani.getBreed());
+        Assert.assertEquals("Getage is incorrect" , 12, ani.getAge());
+        Assert.assertEquals("toString is incorrect", "A 12-year old doggo.", ani.toString());
+    }
 }
