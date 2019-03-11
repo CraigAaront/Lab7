c3ddf02a716fa6bbc64da8aaf3efefccf8295344
Aaron Craig <aaron.t.craig-1@ou.edu>
2019-03-11 10:04:19 -0500

    finished persontests

--- src/PersonTest.java
+++ src/PersonTest.java
@@ -18,4 +18,19 @@ public class PersonTest
         Assert.assertEquals("comparetoImpl incorrect", 0, sicko.compareTo(normie));
         Assert.assertEquals("toString incorrect", "sickomode, a 12-year old. Severity level 1", sicko.toString());
     }
+    @Test
+    public void healthyPersonTest() {
+        Person healthy = new HealthyPerson("aletter", 12, "raison");
+        Person healthy2 = new HealthyPerson("bletter", 13, "raison");
+        Person healthy3 = new HealthyPerson("aletter", 13, "rasion");
+        Person sicko = new SickPerson("normine", 13, 1);
+        
+        Assert.assertEquals("getName is incorrect", "aletter", healthy.getName());
+        Assert.assertEquals("Getage is incorrect", 12, healthy.getAge());
+        Assert.assertEquals("comparetoImpl incorrect", -1, healthy.compareTo(healthy2));
+        Assert.assertEquals("comparetoImpl incorrect", 1, healthy2.compareTo(healthy));
+        Assert.assertEquals("comparetoImpl incorrect", 0, healthy.compareTo(healthy3));
+        Assert.assertEquals("comparetoImpl incorrect", 0, healthy.compareTo(sicko));
+        Assert.assertEquals("toString incorrect", "aletter, a 12-year old. In for raison", healthy.toString());
+    }
 }
